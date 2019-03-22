815c7eeaf5c722a4bc3d95f2fe5afe5ab90eb9b8
PhilOfCalth <philiplarkin8383@gmail.com>
2019-03-22 20:23:57 +0000

    tests for allowed hosts and some minor tweaks

--- albionWeb/albionWeb/settings/production.py
+++ albionWeb/albionWeb/settings/production.py
@@ -2,4 +2,4 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["ec2-*.eu-west-1.compute.amazonaws.com"] # or ['*']
+ALLOWED_HOSTS = ['*'] #["ec2-*.eu-west-1.compute.amazonaws.com"] # or ['*']
--- albionWeb/newsHome/views.py
+++ albionWeb/newsHome/views.py
@@ -10,70 +10,19 @@ _all_articles = None
 def _init_articles():
     global _all_articles
 
-    if None == _all_articles :
-        amazon_publisher = AmazonPublisher()
-        _all_articles = amazon_publisher.retrieve_messages_from_queue(10)
-
-    # article_1 = {"MessageAttributes":
-    #     {
-    #         "captured_date": {"StringValue": "2019-Mar-02", "DataType": "String"},
-    #         "link": {"StringValue": "http://www.modiphius.com/modiphius-press-releases/forbidden-lands-launches",
-    #                  "DataType": "String"},
-    #         "title": {"StringValue": "Forbidden Lands Launches!", "DataType": "String"},
-    #         "website": {"StringValue": "https://www.modiphius.com/4/feed", "DataType": "String"}
-    #     },
-    #     "MessageBody": "Modiphius has announced the release of a brand new RPG, Forbidden Lands. This is new take on classic fantasy roleplaying. In this open-world survival roleplaying game, you’re not heroes sent on missions dictated by others - instead, you are raiders and rogues bent on making your own mark on a cursed world. ",
-    # }
-    #
-    # article_2 = {"MessageAttributes":
-    #     {
-    #         "captured_date": {"StringValue": "2019-Mar-01", "DataType": "String"},
-    #         "link": {"StringValue": "http://www.google.co.uk",
-    #                  "DataType": "String"},
-    #         "image": {"DataType": "String",
-    #                   "StringValue": "https://koboldpress.com/wp-content/uploads/2019/01/WL009_WorldTree-1-193x300.jpg"},
-    #         "title": {"StringValue": "This is google!", "DataType": "String"},
-    #         "website": {"StringValue": "http://www.google.co.uk", "DataType": "String"}
-    #     },
-    #     "MessageBody": "Google google google!",
-    # }
-    #
-    # article_3 = {"MessageAttributes":
-    #     {
-    #         "captured_date": {"StringValue": "2019-Mar-01", "DataType": "String"},
-    #         "link": {"StringValue": "https://www.fantasyflightgames.com/en/news/2019/2/25/a-phantom-menace/",
-    #                  "DataType": "String"},
-    #         "image": {"DataType": "String",
-    #                   "StringValue": "https://images-cdn.fantasyflightgames.com/filer_public/47/c7/47c7503e-03d8-4445-89e8-1987d3d7cb89/swz30_preview1.jpg"},
-    #         "title": {"StringValue": "X wing second edition stuff!", "DataType": "String"},
-    #         "website": {"StringValue": "http://www.google.co.uk", "DataType": "String"}
-    #     },
-    #     "MessageBody": "Well done referencing the worst movie ever!",
-    # }
-    #
-    # _all_articles = [article_1, article_2, article_3]
-    #
-    # for i in range(1000):
-    #     _all_articles.append({"MessageAttributes":
-    #         {
-    #             "captured_date": {"StringValue": "2019-Mar-01", "DataType": "String"},
-    #             "link": {"StringValue": "https://www.fantasyflightgames.com/en/news/2019/2/25/a-phantom-menace/",
-    #                      "DataType": "String"},
-    #             "image": {"DataType": "String",
-    #                       "StringValue": "https://images-cdn.fantasyflightgames.com/filer_public/47/c7/47c7503e-03d8-4445-89e8-1987d3d7cb89/swz30_preview1.jpg"},
-    #             "title": {"StringValue": "X wing second edition stuff!", "DataType": "String"},
-    #             "website": {"StringValue": "http://www.google.co.uk", "DataType": "String"}
-    #         },
-    #         "MessageBody": f"Well done referencing the worst movie ever! # {i} #",
-    #     })
+    #if None == _all_articles :
+    amazon_publisher = AmazonPublisher()
+    _all_articles = amazon_publisher.retrieve_messages_from_queue(10)
 
 
 # Create your views here.
 def home_page(request):
 
-    article_number = request.GET.get('article_number', 1)
+    article_number = request.GET.get('article_number')
 
-    _init_articles()
+    if article_number is None:
+        _init_articles()
+        article_number = 1
 
     paginator = Paginator(_all_articles, 5)
 
@@ -146,7 +95,10 @@ class AmazonPublisher(object):
             WaitTimeSeconds=0
         )
 
-        return response['Messages']
+        messages = response.get('Messages')
+        if messages is None:
+            return []
+        return messages
 
     def delete_message(self, receipt_handle: str):
         # Delete received message from queue
