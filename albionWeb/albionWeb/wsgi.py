5e0c1a340bad4a3b915953f5d6debca34f33666c
PhilOfCalth <philiplarkin8383@gmail.com>
2019-03-22 21:26:36 +0000

    uwsgi tweaking

--- albionWeb/albionWeb/wsgi.py
+++ albionWeb/albionWeb/wsgi.py
@@ -7,7 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+# add the hellodjango project path into the sys.path
+sys.path.append('~/AlbionWeb/albionWeb')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('~/.local/lib/python3.6/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
