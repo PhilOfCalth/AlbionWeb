b5e11b5a02f69896dc4ad2f452a409d38c888cd3
PhilOfCalth <philiplarkin8383@gmail.com>
2019-04-23 00:11:12 +0100

    switch to dynamoDb for actualNews store

---
+++ albionWeb/newsHome/amazonPublisher.py
@@ -0,0 +1,42 @@
+import boto3
+import os
+from boto3.dynamodb.conditions import Key, Attr
+from datetime import datetime
+
+class AmazonPublisher(object):
+    def __init__(self):
+        asw_key = os.environ['albionWeb.userKey']
+        aws_pass = os.environ['albionWeb.userPass']
+
+        self.dynamo_table = boto3.resource('dynamodb', region_name='eu-west-1', aws_access_key_id=asw_key,
+                                aws_secret_access_key=aws_pass)\
+                            .Table('news')
+
+    def put_item_in_dynamo(self, item : map):
+        response = self.dynamo_table.put_item(
+            Item=item
+        )
+
+    def retrieve_messages_from_dynamodb(self, start_time : int,quantity: int = 1) -> list:
+#        response = self.bck_dynamo_table.scan(FilterExpression=Key('date_published').lt(start_time), Limit=quantity)
+        response = self.dynamo_table.query(KeyConditionExpression=Key('type').eq("actualNews") & Key('date_published').lt(start_time),
+                                           ScanIndexForward=False,
+                                           Limit=quantity)
+        items = response[u'Items']
+        if items is None:
+            return []
+        return items
+
+
+if __name__ == "__main__":
+    import time
+
+    article_timestamp = int(time.time() * 1000000)
+    #article_timestamp = 1555708244838 #1555707950386 #1555707512954
+    amazon_pub = AmazonPublisher()
+    items = amazon_pub.retrieve_messages_from_dynamodb(article_timestamp, 3)
+    print("done")
+    # for item in items:
+    #     # del item["id"]
+    #     # item['type'] = "actualNews"
+    #     amazon_pub.put_item_in_dynamo(item)
--- albionWeb/newsHome/views.py
+++ albionWeb/newsHome/views.py
@@ -1,109 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import boto3
-from datetime import datetime
+from .amazonPublisher import AmazonPublisher
+import time
+
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-_all_articles = None
-
-def _init_articles():
-    global _all_articles
-
-    #if None == _all_articles :
-    amazon_publisher = AmazonPublisher()
-    _all_articles = amazon_publisher.retrieve_messages_from_queue(10)
+amazon_publisher = AmazonPublisher()
 
 
 # Create your views here.
 def home_page(request):
 
-    article_number = request.GET.get('article_number')
+    article_timestamp = request.GET.get('article_timestamp')
 
-    if article_number is None:
-        _init_articles()
-        article_number = 1
+    if article_timestamp is None:
+        article_timestamp = int(time.time() * 1000000)
+    else :
+        article_timestamp = int(article_timestamp)
 
-    paginator = Paginator(_all_articles, 5)
+    display_articles = amazon_publisher.retrieve_messages_from_dynamodb(article_timestamp,  5)
 
-    try:
-        display_articles = paginator.page(article_number)
-    except PageNotAnInteger:
-        display_articles = paginator.page(1)
-    except EmptyPage:
-        display_articles = paginator.page(paginator.num_pages)
+    if len(display_articles) == 0 :
+        last_timestamp = -1
+    else:
+        last_timestamp = display_articles[-1]['date_published']
 
-    return render(request, "newsHome/home.html", {'articles': display_articles})
-
-
-class AmazonPublisher(object):
-    def __init__(self):
-        self.sqs = boto3.client('sqs', region_name='eu-west-1', aws_access_key_id="AKIAIVKPXFTVTPS7JAMQ",
-                                aws_secret_access_key="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")
-        self.queue_url = 'https://sqs.eu-west-1.amazonaws.com/185394215596/AlbionActualNewsQueue.fifo'
-
-    def add_message_to_queue(self, title: str, blurb: str, link: str, image: str, website: str) -> str:
-
-        filled_title = title if None is not title and '' != title else "N/A"
-        filled_image = image if None is not image and '' != image else "N/A"
-        filled_blurb = blurb if None is not blurb and '' != blurb else "N/A"
-
-        # Send message to SQS queue
-        response = self.sqs.send_message(
-            QueueUrl=self.queue_url,
-            DelaySeconds=10,
-            MessageAttributes={
-                'title': {
-                    'DataType': 'String',
-                    'StringValue': filled_title
-                },
-                'link': {
-                    'DataType': 'String',
-                    'StringValue': link
-                },
-                'image': {
-                    'DataType': 'String',
-                    'StringValue': filled_image
-                },
-                'website': {
-                    'DataType': 'String',
-                    'StringValue': website
-                },
-                'captured_date': {
-                    'DataType': 'String',
-                    'StringValue': datetime.now().strftime("%Y-%b-%d")
-                }
-            },
-            MessageBody=(
-                filled_blurb
-            )
-        )
-
-        return response['MessageId']
-
-    def retrieve_messages_from_queue(self, quantity: int = 1) -> list:
-        response = self.sqs.receive_message(
-            QueueUrl=self.queue_url,
-            AttributeNames=[
-                'SentTimestamp'
-            ],
-            MaxNumberOfMessages=quantity,
-            MessageAttributeNames=[
-                'All'
-            ],
-            VisibilityTimeout=0,
-            WaitTimeSeconds=0
-        )
-
-        messages = response.get('Messages')
-        if messages is None:
-            return []
-        return messages
-
-    def delete_message(self, receipt_handle: str):
-        # Delete received message from queue
-        if None is not receipt_handle:
-            self.sqs.delete_message(
-                QueueUrl=self.queue_url,
-                ReceiptHandle=receipt_handle
-            )
+    return render(request, "newsHome/home.html", {'articles': display_articles, 'last_timestamp': last_timestamp})
